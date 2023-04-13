578422412dbe396a5089a26da68a0b4ccc625e63
laladean <laurendeano@gmail.com>
2023-04-13 10:40:52 -0600

    detail page and filtering

---
+++ Components/SEXViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using INTEX.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace INTEX.Components
+{
+    public class SEXViewComponent : ViewComponent
+    {
+        private IIntexRepository repo { get; set; }
+        public SEXViewComponent(IIntexRepository temp)
+        {
+            repo = temp;
+        }
+        public IViewComponentResult Invoke()
+        {
+
+            ViewBag.SelectedSEX = RouteData?.Values["bSex"];
+
+            var sex = repo.burialmain.Select(x => x.sex)
+                .Distinct()
+                .OrderBy(x => x);
+
+            return View(sex);
+        }
+    }
+}
