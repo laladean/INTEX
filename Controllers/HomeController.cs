0accf240a8d789f05b4e2bb4bfdc1a4ee80652d5
zackgoodson <zackgoodson@gmail.com>
2023-04-13 11:06:59 -0600

    branch rubbish?

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,18 +6,43 @@ namespace INTEX.Controllers
 {
     public class HomeController : Controller
     {
-
-        private intex2Context context { get; set; }
-        public HomeController(intex2Context temp) => context = temp;
-        public IActionResult Index()
-
         private IIntexRepository repo;
 
-
-        public HomeController (IIntexRepository temp)
-
+        public HomeController(IIntexRepository temp)
         {
-            var x = context.burialmain.ToList();
+            repo = temp;
+        }
+
+        public IActionResult Burials(string hairColor, string ageAtDeath, string burialDepth, int pageNum = 1)
+        {
+            int pageSize = 100;
+
+            var x = new BurialsViewModel
+            {
+                burials = repo.burialmain
+                .Where(b => (hairColor == null || b.haircolor == hairColor) &&
+                (ageAtDeath == null || b.ageatdeath == ageAtDeath) &&
+                (burialDepth == null || b.depth == burialDepth))
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize),
+
+                PageInfo = new PageInfo
+                {
+                    TotalProjects =
+                    ((hairColor == null && ageAtDeath == null)
+                    ? repo.burialmain.Count()
+                    : repo.burialmain
+                        .Where(b => (hairColor == null || b.haircolor == hairColor) &&
+                        (ageAtDeath == null || b.ageatdeath == ageAtDeath) &&
+                        (burialDepth == null || b.depth == burialDepth))
+                        .Count()),
+                    ProjectsPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+
+            };
+
+
             return View(x);
         }
 
@@ -73,10 +98,5 @@ namespace INTEX.Controllers
 
 
 
-
-
-
-
->>>>>>> Stashed changes
     }
 }
